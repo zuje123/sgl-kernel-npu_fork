6b79a4aeaee8408af2b9826b0c7799912f6c40fa
ZFA <92495905+zuje123@users.noreply.github.com>
2025-08-27 19:18:04 +0800

    fix combine memory problem (#54)
    * scale the size of globalbs for normal dispath and combine
    
    * set hccl buffersize for ut
    
    ---------
    
    Co-authored-by: zhengfaan <zhengfaan@outlook.com>

--- scripts/prepare_deepep_in_container.sh
+++ scripts/prepare_deepep_in_container.sh
@@ -3,4 +3,3 @@ bash build.sh
 pip install ${GITHUB_WORKSPACE}/output/deep_ep*.whl --no-cache-dir
 cd "$(pip show deep-ep | awk '/^Location:/ {print $2}')"
 ln -s deep_ep/deep_ep_cpp*.so || true
-export HCCL_BUFFSIZE=2048
