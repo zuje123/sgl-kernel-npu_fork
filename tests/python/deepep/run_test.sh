fb1625f9d5a54bb90db831bc9b5c6334c643494b
z00799692 <zhengfaan@outlook.com>
2025-10-10 17:47:02 +0800

    debug with test

---
+++ tests/python/deepep/run_test.sh
@@ -0,0 +1,31 @@
+SKN_PWD="/home/z00799692/code/1010/sgl-kernel-npu_a2"
+RANK0_IP="61.47.2.182"
+IP=$(hostname -I | awk '{print $1}')
+cd ${SKN_PWD}
+# if [ "$1" != "0" ]; then
+#   bash build.sh
+#   pip uninstall -y deep-ep
+#   pip install "${SKN_PWD}/output/deep_ep-*.whl"
+#   cd "$(pip show deep-ep | awk '/^Location:/ {print $2}')"
+#   ln -s deep_ep/deep_ep_cpp*.so || true
+#   cd -
+# fi
+cd ./tests/python/deepep
+export WORLD_SIZE=2
+export HCCL_BUFFSIZE=1024
+export HCCL_INTRA_PCIE_ENABLE=1
+export HCCL_INTRA_ROCE_ENABLE=0
+rm -rf ./logs
+export ASCEND_PROCESS_LOG_PATH=./logs
+export ASCEND_GLOBAL_LOG_LEVEL=0
+
+export MASTER_ADDR=${RANK0_IP}
+if [ "${IP}" == "${RANK0_IP}" ]; then
+  echo "env rank 0"
+  export RANK=0
+else
+  echo "env rank 1"
+  export RANK=1
+fi
+
+python a2_test_intranode.py
