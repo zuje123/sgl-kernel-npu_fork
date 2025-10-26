05abbed235b3d79a39341ff03fbb557780e9f994
z00799692 <zhengfaan@outlook.com>
2025-10-26 19:02:44 +0800

    fix codespell

--- csrc/deepep/ops2/op_host/op_api/aclnn_moe_distribute_combine_a2.h
+++ csrc/deepep/ops2/op_host/op_api/aclnn_moe_distribute_combine_a2.h
@@ -75,7 +75,7 @@ __attribute__((visibility("default"))) aclnnStatus aclnnMoeDistributeCombineA2Ge
  * @param [in] workspace: 在npu device侧申请的workspace内存起址。
  * @param [in] workspace_size: 在npu
  * device侧申请的workspace大小，由第一段接口aclnnMoeDistributeCombineGetWorkspaceSize获取。
- * @param [in] exector: op执行器，包含了算子计算流程。
+ * @param [in] executor: op执行器，包含了算子计算流程。
  * @param [in] stream: acl stream流。
  * @return aclnnStatus: 返回状态码
  */
