cdf8b0b80812db7ac34a5ead0eafcd9f6b3b27a4
McZyWu <zhuoyun.wu.23@ucl.ac.uk>
2026-02-28 08:54:19 +0800

    revise causal_conv1d: bugfix and enhance accuracy for model kimilinear (#370)
    * revise causal_conv1d:enhance accuracy for model kimilinear

--- python/sgl_kernel_npu/sgl_kernel_npu/mamba/causal_conv1d.py
+++ python/sgl_kernel_npu/sgl_kernel_npu/mamba/causal_conv1d.py
@@ -586,6 +586,7 @@ def torch_causal_conv1d_update_npu(
     out = torch.sum(hidden_states_new * weight, dim=-1, keepdim=True)
     out = F.silu(out)
     out = out.to(hidden_state.dtype)
+    conv_state_update = conv_state_update.to(hidden_state.dtype)
     return out, conv_state_update
 
 
