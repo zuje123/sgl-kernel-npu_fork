8c2a820280595f1db55afc1d792f5f553343b330
zhengfaan <zhengfaan@outlook.com>
2026-03-02 21:22:56 +0800

    fix compile for kernel with catlass

--- csrc/deepep/ops/build.sh
+++ csrc/deepep/ops/build.sh
@@ -17,6 +17,43 @@ fi
 echo "using ASCEND_HOME_PATH: $ASCEND_HOME_PATH"
 script_path=$(realpath $(dirname $0))
 
+# 创建third_party目录（如果不存在）
+mkdir -p "${SCRIPTS_DIR}/third_party"
+CATLASS_DIR="${SCRIPTS_DIR}/third_party/catlass"
+
+# ASCEND910C (A3) series
+# dependency: catlass
+git config --global --add safe.directory "$SCRIPTS_DIR"
+CATLASS_PATH=${CATLASS_DIR}/include
+if [[ ! -d "${CATLASS_PATH}" ]]; then
+    echo "dependency catlass is missing, try to fetch it..."
+    if ! git clone -b catlass-v1-stable https://gitcode.com/cann/catlass.git "${CATLASS_DIR}"; then
+        echo "catlass fetch failed"
+        exit 1
+    fi
+fi
+# dependency: cann-toolkit file moe_distribute_base.h
+HCCL_STRUCT_FILE_PATH=$(find -L "${ASCEND_HOME_PATH}" -name "moe_distribute_base.h" 2>/dev/null | head -n1)
+if [ -z "$HCCL_STRUCT_FILE_PATH" ]; then
+    echo "cannot find moe_distribute_base.h file in CANN env"
+    exit 1
+fi
+# for dispatch & combine..
+yes | cp "$HCCL_STRUCT_FILE_PATH" "$SCRIPTS_DIR/op_kernel/"
+
+# for dispatch_ffn_combine & dispatch_ffn_combine_bf16
+TARGET_DIR="$SCRIPTS_DIR/op_kernel/dispatch_ffn_combine_kernel/utils/"
+TARGET_FILE="$TARGET_DIR/$(basename "$HCCL_STRUCT_FILE_PATH")"
+# TARGET_DIR_BF16="$SCRIPT_DIR_BF16/dispatch_ffn_combine_bf16/op_kernel/utils/"
+# TARGET_FILE_BF16="$TARGET_DIR_BF16/$(basename "$HCCL_STRUCT_FILE_PATH")"
+echo "*************************************"
+echo $HCCL_STRUCT_FILE_PATH
+echo "$TARGET_DIR"
+cp "$HCCL_STRUCT_FILE_PATH" "$TARGET_DIR"
+sed -i 's/struct HcclOpResParam {/struct HcclOpResParamCustom {/g' "$TARGET_FILE"
+sed -i 's/struct HcclRankRelationResV2 {/struct HcclRankRelationResV2Custom {/g' "$TARGET_FILE"
+
+
 BUILD_DIR="build_out"
 HOST_NATIVE_DIR="host_native_tiling"
 
